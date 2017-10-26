4a1938147fef7844f0a72eace7b4d765c4b99d60
Ayushi Solanki <ayupsol1@gmail.com>
2017-10-26 15:44:57 +0530

    Add files via upload

--- production/index.php
+++ production/index.php
@@ -1,53 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>MathTool | </title>
+
+    <!-- Bootstrap -->
+    <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="../vendors/animate.css/animate.min.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="../build/css/custom.min.css" rel="stylesheet">
+  </head>
+
+  <body class="login">
+    <div>
+      <a class="hiddenanchor" id="signup"></a>
+      <a class="hiddenanchor" id="signin"></a>
+
+      <div class="login_wrapper">
+        <div class="animate form login_form">
+          <section class="login_content" style="margin-top: 10%;">
+          
+              <h1>Login with gmail</h1>
+              
+
+              <div class="clearfix"></div>
 <?php
 //Include GP config file && User class
 include_once 'gpConfig.php';
 include_once 'User.php';
 
 if(isset($_GET['code'])){
-	$gClient->authenticate($_GET['code']);
-	$_SESSION['token'] = $gClient->getAccessToken();
-	header('Location: ' . filter_var($redirectURL, FILTER_SANITIZE_URL));
+  $gClient->authenticate($_GET['code']);
+  $_SESSION['token'] = $gClient->getAccessToken();
+  header('Location: ' . filter_var($redirectURL, FILTER_SANITIZE_URL));
 }
 
 if (isset($_SESSION['token'])) {
-	$gClient->setAccessToken($_SESSION['token']);
+  $gClient->setAccessToken($_SESSION['token']);
 }
 
 if ($gClient->getAccessToken()) {
-	//Get user profile data from google
-	$gpUserProfile = $google_oauthV2->userinfo->get();
-	
-	//Initialize User class
-	$user = new User();
-	
-	//Insert or update user data to the database
+  //Get user profile data from google
+  $gpUserProfile = $google_oauthV2->userinfo->get();
+  
+  //Initialize User class
+  $user = new User();
+  
+  //Insert or update user data to the database
     $gpUserData = array(
         'oauth_provider'=> 'google',
         'oauth_uid'     => $gpUserProfile['id'],
         'first_name'    => $gpUserProfile['given_name'],
         'last_name'     => $gpUserProfile['family_name'],
         'email'         => $gpUserProfile['email'],
-        'gender'        => $gpUserProfile['gender'],
+        //'gender'        => $gpUserProfile['gender'],
         'locale'        => $gpUserProfile['locale'],
         'picture'       => $gpUserProfile['picture'],
-        'link'          => $gpUserProfile['link']
+        //'link'          => $gpUserProfile['link']
     );
     $userData = $user->checkUser($gpUserData);
-	
-	//Storing user data into session
-	$_SESSION['userData'] = $userData;
-	
-	//Render facebook profile data
+  
+  //Storing user data into session
+  $_SESSION['userData'] = $userData;
+  
+  //Render facebook profile data
     if(!empty($userData)){
         $output = '<h1>Google+ Profile Details </h1>';
         $output .= '<img src="'.$userData['picture'].'" width="300" height="220">';
         $output .= '<br/>Google ID : ' . $userData['oauth_uid'];
         $output .= '<br/>Name : ' . $userData['first_name'].' '.$userData['last_name'];
         $output .= '<br/>Email : ' . $userData['email'];
-        $output .= '<br/>Gender : ' . $userData['gender'];
+       // $output .= '<br/>Gender : ' . $userData['gender'];
         $output .= '<br/>Locale : ' . $userData['locale'];
         $output .= '<br/>Logged in with : Google';
-        $output .= '<br/><a href="'.$userData['link'].'" target="_blank">Click to Visit Google+ Page</a>';
+       // $output .= '<br/><a href="'.$userData['link'].'" target="_blank">Click to Visit Google+ Page</a>';
         $output .= '<br/>Logout from <a href="logout.php">Google</a>';
         $output .= '<br/>Logout from <a href="index1.php">home</a>';
         $output .= '<br/>Logout from <a href="home.php">me</a>';
@@ -55,19 +92,18 @@ if ($gClient->getAccessToken()) {
         $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
     }
 } else {
-	$authUrl = $gClient->createAuthUrl();
-	$output = '<a href="'.filter_var($authUrl, FILTER_SANITIZE_URL).'"><img src="images/glogin.png" alt=""/></a>';
+  $authUrl = $gClient->createAuthUrl();
+  $output = '<a href="'.filter_var($authUrl, FILTER_SANITIZE_URL).'"><img src="images/glogin.png" alt=""/ style="max-width:350px; max-height:522px;"></a>';
 }
 ?>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Login with Google using PHP by CodexWorld</title>
-<style type="text/css">
-h1{font-family:Arial, Helvetica, sans-serif;color:#999999;}
-</style>
-</head>
-<body>
+
 <div><?php echo $output; ?></div>
-</body>
+<p style="margin-top: 20px;"><strong >Math Tool!</strong>   </p>         
+          </section>
+        </div>
+
+        
+      </div>
+    </div>
+  </body>
 </html>
